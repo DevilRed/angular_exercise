eb80cbc440c56af322ea0aec8d46f8d54a1abfe8
Christian <christian.xperius@gmail.com>
2017-01-30 12:36:14 -0400

    added headers for product list

--- js/index.js
+++ js/index.js
@@ -4,13 +4,16 @@ angular.module("shoppingCart")
 
     this.items = productService.getProducts();
 
-
     this.remove = function(index) {
         this.items.splice(index, 1);
-    }
+    };
 }])
 
 .controller('addController', ['$scope', 'productService', function ($scope, productService){
 	this.title = 'Add Products form';
-	this.offeredProducts = productService.getProducts();
+	this.offeredProducts = productService.getOfferedProducts();
+
+	this.save = function ($event){
+    	$event.preventDefault();
+    };
 }]);
--- js/services.js
+++ js/services.js
@@ -1,5 +1,14 @@
 angular.module("shoppingCart")
 	.service('productService', [function (){
+		this.getOfferedProducts = function (){
+			var products = [
+				{title: 'TV', stock: 10, price: 500},
+				{title: 'Radio', stock: 10, price: 80},
+				{title: 'Microwave', stock: 10, price: 150},
+				{title: 'ChromeCast', stock: 10, price: 70},
+			];
+			return products;
+		};
 		this.getProducts = function (){
 			var products = [
 				{title: 'TV', quantity: 1, price: 500},
