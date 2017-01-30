59d4f4d6e2f313ae0e4e53ae423f5458eab4ea1a
Christian <christian.xperius@gmail.com>
2017-01-30 11:34:40 -0400

    productService used

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-var shoppingCart = angular.module("shoppingCart", []);
+angular.module("shoppingCart")
 
-shoppingCart.controller("CartController", ['$scope', function($scope){
+.controller("CartController", ['productService', '$scope', function(productService, $scope){
 
-    $scope.items = [
+    /*$scope.items = [
         {title: 'TV', quantity: 1, price: 500},
         {title: 'Radio', quantity: 1, price: 80}
-    ];
+    ];*/
+    this.items = productService.getProducts();
 
     $scope.remove = function(index) {
         $scope.items.splice(index, 1);
     }
-}]);
+}])
 
-shoppingCart.controller('addController', ['$scope', function ($scope){
+.controller('addController', ['$scope', function ($scope){
 	this.title = 'Add Products form';
 }]);
--- js/services.js
+++ js/services.js
@@ -1,10 +1,11 @@
-angular.module("shoppingCart", [])
-	.service('productService', ['$scope', function ($scope){
+angular.module("shoppingCart")
+	.service('productService', [function (){
 		this.getProducts = function (){
 			var products = [
 				{title: 'TV', quantity: 1, price: 500},
 				{title: 'Radio', quantity: 1, price: 80}
 			];
+			// console.log(products);
 			return products;
 		};
 	}])
