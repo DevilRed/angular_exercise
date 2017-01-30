add622cf18b9e70d7bc37b4f223fb62085523f0f
Christian <christian.xperius@gmail.com>
2017-01-30 19:47:50 -0400

    progress

--- js/services.js
+++ js/services.js
@@ -10,24 +10,45 @@ angular.module("shoppingCart")
 				{title: 'TV', quantity: 1, price: 500},
 				{title: 'Radio', quantity: 1, price: 80}
 			];
+			// $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Learn AngularJS', done: false}, {text: 'Build an Angular app', done: false} ];
+		var soldProducts = (localStorage.getItem('sold_products')!==null) ? JSON.parse(localStorage.getItem('sold_products')) : [
+				{title: 'TV', quantity: 1, price: 500},
+				{title: 'Radio', quantity: 1, price: 80}
+			];
 		this.getOfferedProducts = function (){
 			return products;
 		};
 		this.getSoldProducts = function (){
+			// console.log(soldProducts);
+			soldProducts = JSON.parse(localStorage.getItem('sold_products'))
 			return soldProducts;
 		};
 		this.addProduct = function (product){
 			// console.log(product.title);
 			var flag = false;
+			var toEdit;
+			var soldProducts = fetch();
 			angular.forEach(soldProducts, function (val, key){
-				// console.log(val.title);
+				console.log(val.title);
 				if(val.title === product.title) {
 					flag = true;
-					val.quantity = product.quantity;
+					toEdit = key;
 				}
 			});
-			if(flag === false)
+			if(flag === false ){
 				soldProducts.push(product);
+				console.log(soldProducts);
+				flag = true;
+				localStorage.setItem('sold_products', JSON.stringify(soldProducts));
+			} else{
+				console.log(toEdit);
+				soldProducts[toEdit].quantity = product.quantity;
+				var toEdit;
+				product = {};
+			}
+		};
+		function fetch(){
+			return soldProducts;
 		};
 	}])
 	;
