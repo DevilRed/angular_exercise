b1797ff20d1e3ce7f10edb8ac157d5fa4c84e07c
Christian <christian.xperius@gmail.com>
2017-01-30 12:12:56 -0400

    showed form for adding products

--- js/index.js
+++ js/index.js
@@ -2,17 +2,15 @@ angular.module("shoppingCart")
 
 .controller("CartController", ['productService', '$scope', function(productService, $scope){
 
-    /*$scope.items = [
-        {title: 'TV', quantity: 1, price: 500},
-        {title: 'Radio', quantity: 1, price: 80}
-    ];*/
     this.items = productService.getProducts();
+
 
     $scope.remove = function(index) {
         $scope.items.splice(index, 1);
     }
 }])
 
-.controller('addController', ['$scope', function ($scope){
+.controller('addController', ['$scope', 'productService', function ($scope, productService){
 	this.title = 'Add Products form';
+	this.offeredProducts = productService.getProducts();
 }]);
